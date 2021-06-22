4532df389a17194e9415974036f4d8970d790b58
BCaroline01 <bony.caroline@gmail.com>
2021-06-22 15:54:47 +0200

    insertion paticipant and tounament done

--- API/controllers/ControllerParticipant.php
+++ API/controllers/ControllerParticipant.php
@@ -6,15 +6,31 @@ class ControllerParticipant
     public function insertParticipant()
     {
 
-        $insert = new Participant(array('firstname' => $_POST['firstname'], 'lastname' => $_POST['lastname'], 'dob' => $_POST['dob'], 'email' => $_POST['email'], 'picture' => $_FILES['picture'], 'id_category' => $_POST['category']));
-        foreach ($insert as $key => $value) {
-        
-            
-        }
+        $nb = count($_POST['lastname']);
+        for($i=0; $i<$nb; $i++){
+            $insert = new Participant(array('firstname' => $_POST['firstname'][$i], 'lastname' => $_POST['lastname'][$i], 'dob' => $_POST['dob'][$i], 'email' => $_POST['email'][$i], 'picture' => $_FILES['picture']['name'][$i], 'id_category' => $_POST['category'][$i]));
+
             $manager = new ParticipantManager();
-            $participant = $manager->insertionParticipant($key);
+            $participant = $manager->insertionParticipant($insert); 
+        }
+       
+    }
+
+    public function insertExcel(){
         
-        var_dump($participant);
+        require 'vendor/autoload.php';
+
+            use PhpOffice\PhpSpreadsheet\Spreadsheet;
+            use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+            $spreadsheet = new Spreadsheet();
+            $sheet = $spreadsheet->getActiveSheet();
+            $sheet->setCellValue('A1', 'coucou !');
+
+            $writer = new Xlsx($spreadsheet);
+            $writer->save('hello.xlsx');
+
+            echo "<meta http-equiv='refresh' content='0;url=hello.xlsx'/>";
     }
 
     public function listParticipant()
